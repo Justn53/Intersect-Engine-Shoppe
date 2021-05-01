d8ef57e35227f8d71f3495ae58333537c876b8ea
BiggestShoelaces <cooney383@gmail.com>
2021-05-01 13:03:29 -0600

    Update Entity.cs
    Added byte Running and set running speed

--- Intersect.Server/Entities/Entity.cs
+++ Intersect.Server/Entities/Entity.cs
@@ -791,6 +791,10 @@ namespace Intersect.Server.Entities
         public virtual float GetMovementTime()
         {
             var time = 1000f / (float)( 1 + Math.Log( Stat[(int)Stats.Speed].Value() ) );
+            if( Running )
+            {
+                time *= 0.5f;
+            }
             if( Blocking )
             {
                 time += time * Options.BlockingSlow;
